c45f4d55802e6f0d555800d8246858c34ac26949
Nigel Charman <nigel.charman.nz@gmail.com>
2016-04-23 18:59:32 +1200

    Modified info log message

--- src/main/java/org/concordion/ext/run/parallel/ParallelRunStrategy.java
+++ src/main/java/org/concordion/ext/run/parallel/ParallelRunStrategy.java
@@ -30,18 +30,18 @@ import com.google.common.util.concurrent.ListeningExecutorService;
 import com.google.common.util.concurrent.MoreExecutors;
 
 /**
- * Runs specifications in parallel. Each specification fixture has its own instance of ParallelRunStrategy. 
- * The {@link #call(Runner, Resource, String, ResultAnnouncer, ResultRecorder)} method is invoked 
- * for each child specification that the fixture executes. 
+ * Runs specifications in parallel. Each specification fixture has its own instance of ParallelRunStrategy.
+ * The {@link #call(Runner, Resource, String, ResultAnnouncer, ResultRecorder)} method is invoked
+ * for each child specification that the fixture executes.
  * (this is called from the RunCommand when a concordion:run command is executed).
- * 
+ *
  * After processing a specification, this class waits for all of the executed child specifications to complete.
- * To avoid thread starvation, it allocates an additional thread while waiting. 
- * 
+ * To avoid thread starvation, it allocates an additional thread while waiting.
+ *
  * The thread pool is shared across all instances.
- * 
+ *
  * Before usage, the {@link #initialise(String)} method must be called to initialise the thread pool to the
- * appropriate size. 
+ * appropriate size.
  */
 public class ParallelRunStrategy implements RunStrategy, SpecificationProcessingListener {
 
@@ -50,18 +50,18 @@ public class ParallelRunStrategy implements RunStrategy, SpecificationProcessing
     private static Object poolSizeLock = new Object();
     private static volatile Resource mainSpecification;
     private static Logger logger = LoggerFactory.getLogger("org.concordion.ext.run.parallel");
-    
+
     private TaskLatch taskLatch = new TaskLatch();
-    
+
     /**
      * Initialises the thread pool.
      * @param runThreadCount the number of threads in the thread pool. If this ends with C, the runThreadCount is
-     * multiplied by the number of cores. For example, a runThreadCount of <code>2.5C</code> will allocate 10 
+     * multiplied by the number of cores. For example, a runThreadCount of <code>2.5C</code> will allocate 10
      * threads when there are 4 processors available to the JVM.
      */
     public static void initialise(String runThreadCount) {
         int threadPoolSize = parseThreadCount(runThreadCount);
-        logger.info("Running concordion:run commands in parallel with {} threads\n", threadPoolSize);
+        logger.info("Initialising ParallelRunStrategy with {} threads\n", threadPoolSize);
         executor = (ThreadPoolExecutor) Executors.newFixedThreadPool(threadPoolSize);
         service = MoreExecutors.listeningDecorator(executor);
     }
@@ -99,20 +99,20 @@ public class ParallelRunStrategy implements RunStrategy, SpecificationProcessing
     public void afterProcessingSpecification(SpecificationProcessingEvent event) {
         waitForCompletion(event.getResource());
     }
-    
+
     private static int parseThreadCount(String threadCount) {
         try {
             if (threadCount.endsWith("C")) {
                 return new BigDecimal(threadCount.substring(0, threadCount.length() - 1)).multiply(new BigDecimal(Runtime.getRuntime().availableProcessors())).intValue();
-            } 
+            }
             return Integer.parseInt(threadCount);
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("If set, the system property '" + ParallelRunExtension.PROPERTY_RUN_THREAD_COUNT 
+            throw new IllegalArgumentException("If set, the system property '" + ParallelRunExtension.PROPERTY_RUN_THREAD_COUNT
                     + "' must be either an integer value, or a numeric value suffixed with C."
                     + " If the latter, the numeric value is multiplied by the number of cores.");
         }
     }
-    
+
     private Callable<ResultSummary> createTask(final Runner runner, final Resource resource, final String href) {
         return new Callable<ResultSummary>() {
             @Override
@@ -126,21 +126,21 @@ public class ParallelRunStrategy implements RunStrategy, SpecificationProcessing
             }
         };
     }
-    
+
     private ListenableFuture<ResultSummary> submitTask(Callable<ResultSummary> task) {
         return service.submit(task);
     }
-    
+
     private void addCallback(ListenableFuture<ResultSummary> future, final Resource resource, final ResultAnnouncer announcer, final ResultRecorder resultRecorder) {
         Futures.addCallback(future, new FutureCallback<ResultSummary>() {
-            
+
             @Override
             public void onSuccess(ResultSummary runnerResult) {
                 announcer.announce(runnerResult);
                 resultRecorder.record(runnerResult);
                 taskLatch.markTaskComplete();
             }
-            
+
             @Override
             public void onFailure(Throwable t) {
                 if (t.getCause() instanceof FailFastException) {
@@ -154,10 +154,10 @@ public class ParallelRunStrategy implements RunStrategy, SpecificationProcessing
             }
         });
     }
-    
+
     private void waitForCompletion(Resource resource) {
         if (taskLatch.hasRegisteredTasks()) {
-            // to avoid thread starvation when this thread blocks waiting for its tasks to complete, allocate an extra thread 
+            // to avoid thread starvation when this thread blocks waiting for its tasks to complete, allocate an extra thread
             allocateWaitThread(resource);
             taskLatch.waitForAllTasksToComplete();
             deallocateWaitThread(resource);
@@ -166,7 +166,7 @@ public class ParallelRunStrategy implements RunStrategy, SpecificationProcessing
 
     private void allocateWaitThread(Resource resource) {
         synchronized (poolSizeLock) {
-            if (!resource.equals(mainSpecification)) { 
+            if (!resource.equals(mainSpecification)) {
                 int newPoolSize = executor.getCorePoolSize() + 1;
                 executor.setMaximumPoolSize(newPoolSize);
                 executor.setCorePoolSize(newPoolSize);
@@ -177,7 +177,7 @@ public class ParallelRunStrategy implements RunStrategy, SpecificationProcessing
 
     private void deallocateWaitThread(Resource resource) {
         synchronized (poolSizeLock) {
-            if (!resource.equals(mainSpecification)) { 
+            if (!resource.equals(mainSpecification)) {
                 int newPoolSize = executor.getCorePoolSize() - 1;
                 executor.setCorePoolSize(newPoolSize);
                 executor.setMaximumPoolSize(newPoolSize);
@@ -185,43 +185,43 @@ public class ParallelRunStrategy implements RunStrategy, SpecificationProcessing
         }
         logger.debug("Complete: {}. Total threads: {}", resource, executor.getCorePoolSize());
     }
-    
+
     /**
      * A latch to wait for tasks to complete.
      * This proceeds in 2 distinct phases:
      * 1. New tasks are registered, using {@link #registerTask()}
      * 2. After all tasks are registered, task completion is awaited using {@link #waitForAllTasksToComplete()}
      * {@link #markTaskComplete()} may be called at any time to mark a task as complete.
-     * 
+     *
      * {@link #waitForAllTasksToComplete()} for wait until {@link #markTaskComplete()} has been called the same
      * number of times as {@link #registerTask()}. Before waiting, {@link #hasRegisteredTasks()} may be called
-     * to check whether any tasks were registered. 
-     * 
+     * to check whether any tasks were registered.
+     *
      * This class is thread-safe.
-     * 
-     * In Java 7, the Phaser class would be a replacement for this (see http://stackoverflow.com/a/1637030). 
+     *
+     * In Java 7, the Phaser class would be a replacement for this (see http://stackoverflow.com/a/1637030).
      */
     private static class TaskLatch {
         private AtomicInteger taskCounter = new AtomicInteger(0);
         private AtomicBoolean waiting = new AtomicBoolean(false);
         private Semaphore semaphore = new Semaphore(0);
-        
+
         void registerTask() {
             if (waiting.get()) {
                 throw new IllegalStateException("New tasks not expected when waiting.");
             }
             taskCounter.incrementAndGet();
         }
-        
+
         boolean hasRegisteredTasks() {
             waiting.set(true);
             return taskCounter.get() > 0;
         }
-        
+
         void markTaskComplete() {
             semaphore.release(1);
         }
-        
+
         void waitForAllTasksToComplete() {
             waiting.set(true);
             boolean complete = false;
